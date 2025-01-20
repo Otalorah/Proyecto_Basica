b9a7849546db983e77cb8ff7e1d717e9b673e8b7
Johan Bermeo <johan.bermeo29@outlook.com>
2025-01-20 09:52:38 -0500

    Arreglo función de creación del numero aleatorio

--- functions.cpp
+++ functions.cpp
@@ -2,36 +2,67 @@
 #include <locale>
 #include <stdlib.h>
 #include <random>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
 
 using namespace std;
 
-int num_oculto();
+vector<int> num_oculto();
 void intento();
 void comprobacion();
+vector<int> numAVector (int num);
 
 int main(){
     setlocale(LC_ALL, "spanish");
+    
+    wcout << L"Número oculto: " << endl;
 
-    num_oculto();
-    wcout << L"Número oculto: " << num_oculto() << endl;
+    vector<int>num = num_oculto();
+    for (int i : num) {
+        wcout << i << L" ";  // Imprimir cada dígito
+    }
 
     system("pause");
     return 0;
 }
 
+//Funcion que comprobará el numero ingresado con el numero generado aleatoriamente.
 void comprobacion(){
-    //Funcion que comprobará el numero ingresado con el numero generado aleatoriamente.
 }
 
+//Funcion que permite el ingreso de un numero.
 void intento(){
-    //Funcion que permite el ingreso de un numero.
 }
 
-int num_oculto(){
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dis(0, 99999);
+//Funcion que llena con ceros el numero ingresado en caso de no estar completo
+void ceros(){
 
-    int num = dis(gen);
+}
+
+//Funcion que convierte los numeros a vectores
+vector<int> numAVector (int num){
+    vector<int> digitos;
+    while (num > 0){
+        digitos.push_back(num % 10);
+        num /= 10;
+    } 
+    reverse(digitos.begin(), digitos.end());
+    return digitos;
+}
+
+vector<int> num_oculto(){
+    vector<int> num;
+
+    unsigned seed = static_cast<unsigned>(time(0));
+    mt19937 generador(seed);
+
+    uniform_int_distribution<int>distribucion{0, 9};
+
+    for (int i = 0; i < 5; ++i){
+        int digito = distribucion(generador);
+        num.push_back(digito);
+    }
     return num;
 }
