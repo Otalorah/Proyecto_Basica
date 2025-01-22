78c4826ca77da64a740f91aea0c929787af34262
Johan Bermeo <johan.bermeo29@outlook.com>
2025-01-22 08:21:41 -0500

    Archivo de importaciones =)
    Perdón =P

---
+++ funciones.h
@@ -0,0 +1,8 @@
+#ifndef functions
+#define functions
+
+vector<int> hideNum();
+vector<int> attempt(int num);
+pair<int, int> checkUp(const vector<int>& num, const vector<int>& joinedNum);
+
+#endif
